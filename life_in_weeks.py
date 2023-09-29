5d502fbc8b9be39b917a43138e31db1337977611
Pratik Kumar <93330691+hoodboypk@users.noreply.github.com>
2023-09-29 20:09:31 +0530

    Add files via upload

---
+++ life_in_weeks.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your current age: "))
+
+years_remaining = 90 - age
+
+days_remaining = years_remaining*365
+
+weeks_remaining = years_remaining*52
+
+months_remaining = years_remaining*12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months to live")
